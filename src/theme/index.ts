633183ab18b6f7037a865246fcb688634ec84e58
rishav-jha-mech <rishav.rj74@gmail.com>
2026-03-14 12:20:41 +0530

    lint: changes due to eslint

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,12 @@
 export { tokens } from './tokens/index';
-export type { TTokens, TColorToken, TSpacingToken, TRoundedToken, TFontToken, TFontVariant } from './tokens/index';
+export type {
+  TTokens,
+  TColorToken,
+  TSpacingToken,
+  TRoundedToken,
+  TFontToken,
+  TFontVariant,
+} from './tokens/index';
 export { createTheme, defaultTheme } from './create-theme';
 export type { TTheme } from './create-theme';
 export { ThemeProvider, useTheme } from './provider';
--- src/theme/provider.tsx
+++ src/theme/provider.tsx
@@ -3,7 +3,13 @@ import { TTheme, defaultTheme } from './create-theme';
 
 const ThemeContext = createContext<TTheme>(defaultTheme);
 
-export function ThemeProvider({ theme = defaultTheme, children }: { theme?: TTheme; children: React.ReactNode }) {
+export function ThemeProvider({
+  theme = defaultTheme,
+  children,
+}: {
+  theme?: TTheme;
+  children: React.ReactNode;
+}) {
   return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
 }
 
